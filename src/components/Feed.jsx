ec459601f22d8c76aebe3ff7fa13ea71e63fbdda
ali-jh <alijh9518@yahoo.com>
2023-08-10 02:37:08 +0330

    impelemnt sidebar and add to the Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import {Sidebar} from "../components"
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { xs: "92vh", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar/>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ color: "#fff", mt: 1.5 }}
+        >
+          CopyRight 2024 datamcode
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
